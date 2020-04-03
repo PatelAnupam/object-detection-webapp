076d9021ba6db9f216de897f0d03338a10d72654
PatelAnupam <anupam.patel@brightlifecare.com>
2020-04-03 17:28:28 +0530

    webapp

---
+++ precache-manifest.62bd9283a7a13bdff32c4ca8731e7cfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63cb8ec18a852369e57ecd5b4c4ce48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3f2c8affde71c48d18c",
+    "url": "/static/css/main.68333960.chunk.css"
+  },
+  {
+    "revision": "9b43a2ceb76878f8d5a4",
+    "url": "/static/js/2.4105f8cf.chunk.js"
+  },
+  {
+    "revision": "a3f2c8affde71c48d18c",
+    "url": "/static/js/main.d624afec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
